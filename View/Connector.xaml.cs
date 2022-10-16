f5f1ea71eb6465b6ab21a708fdcbe54c4def41da
MkSerdyuk <git_user@mail.ru>
2022-10-16 23:12:40 +0300

    Написан код создания соединяющей линии

---
+++ View/Connector.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Flowchart_Framework.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flowchart_Framework.View
+{
+    /// <summary>
+    /// Interaction logic for Connector.xaml
+    /// </summary>
+    public partial class Connector : UserControl
+    {
+        private LayoutWatcher _selfWatcher = new LayoutWatcher();
+        private LayoutWatcher _fromWatcher = new LayoutWatcher();
+        private LayoutWatcher _toWatcher = new LayoutWatcher();
+
+        public Connector From
+        {
+            get { return (Connector)GetValue(FromProperty); }
+            set { SetValue(FromProperty, value); }
+        }
+
+        public static readonly DependencyProperty FromProperty =
+        DependencyProperty.Register("From", typeof(FrameworkElement), typeof(Connector),
+        new FrameworkPropertyMetadata((o, args) =>
+        { var self = (Connector)o; self._fromWatcher.ChangeTarget(self.From); }));
+
+        public Connector To
+        {
+            get { return (Connector)GetValue(ToProperty); }
+            set { SetValue(ToProperty, value); }
+        }
+
+        public static readonly DependencyProperty ToProperty =
+            DependencyProperty.Register("To", typeof(FrameworkElement), typeof(Connector),
+                new FrameworkPropertyMetadata((o, args) =>
+                { var self = (Connector)o; self._toWatcher.ChangeTarget(self.To); }));
+
+        public Connector()
+        {
+            InitializeComponent();
+
+            _selfWatcher.ChangeTarget(this);
+
+            _selfWatcher.Changed += RedrawLine;
+            _fromWatcher.Changed += RedrawLine;
+            _toWatcher.Changed += RedrawLine;
+            RedrawLine(null, null);
+        }
+
+        private void RedrawLine(object sender, LayoutChangeEventArgs e)
+        {
+            if (From == null || To == null)
+            {
+                ConnectingLine.Visibility = Visibility.Collapsed;
+                return;
+            }
+
+            ConnectingLine.Visibility = Visibility.Visible;
+
+            var fromRect = LayoutWatcher.ComputeRendererRect(From, this);
+            var toRect = LayoutWatcher.ComputeRendererRect(To, this);
+
+            ConnectingLine.X1 = fromRect.Right + 5;
+            ConnectingLine.Y1 = fromRect.Top + fromRect.Height / 2;
+
+            ConnectingLine.X2 = toRect.Left - 5;
+            ConnectingLine.Y2 = toRect.Top + toRect.Height / 2;
+        }
+    }
+}
---
+++ ViewModels/LayoutWatcher.cs
@@ -0,0 +1,61 @@
+﻿using Flowchart_Framework.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Flowchart_Framework.ViewModels
+{
+    public class LayoutChangeEventArgs : EventArgs
+    {
+        public readonly Rect Rect;
+        public LayoutChangeEventArgs(Rect Rect) => Rect = Rect;
+    }
+    
+    public class LayoutWatcher
+    {
+        private Connector _target, _origin;
+        private Rect _currentRendererRect = Rect.Empty;
+        public event EventHandler<LayoutChangeEventArgs> Changed;
+
+        public void ChangeTarget(Connector target, Connector origin = null)
+        {
+            if (_target != null)
+            {
+                _target.LayoutUpdated -= OnLayoutUpdate;
+            }
+
+            _target = target;
+            _origin = origin;
+            OnLayoutUpdate(null, null);
+
+            if (_target != null)
+            {
+                _target.LayoutUpdated += OnLayoutUpdate;
+            }
+        } 
+
+        private void OnLayoutUpdate(object sender, EventArgs e)
+        {
+            Rect newRendererRect = GetRendererRect();
+            if (newRendererRect != _currentRendererRect)
+            {
+                _currentRendererRect = newRendererRect;
+                Changed?.Invoke(_target, new LayoutChangeEventArgs(_currentRendererRect));
+            }
+        }
+
+        public static Rect ComputeRendererRect(Connector target, Connector origin)
+        {
+            return new Rect(target.TranslatePoint(new Point(), origin), target.RenderSize);
+        }
+
+        private Rect GetRendererRect()
+        {
+            return ComputeRendererRect(_target, _origin);
+        }
+
+    }
+}
